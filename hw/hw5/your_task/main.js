d4fac080f9b3bb392ede885d25bf2eba77857723
Nova <brave012345@gmail.com>
2022-12-06 02:04:14 -0500

    hw5 and project 2 upload

--- hw/hw5/your_task/main.js
+++ hw/hw5/your_task/main.js
@@ -1,6 +1,6 @@
 const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 
-function search (ev) {
+function search(ev) {
     const term = document.querySelector('#search').value;
     console.log('search for:', term);
     // issue three Spotify queries at once...
@@ -12,8 +12,8 @@ function search (ev) {
     }
 }
 
-async function getTracks (term) {
-    const trackEndpoint = baseURL + "?q=" + term +"&type=track";
+async function getTracks(term) {
+    const trackEndpoint = baseURL + "?q=" + term + "&type=track";
     console.log(trackEndpoint)
 
     const data = await fetch(trackEndpoint).then(response => response.json());
@@ -23,9 +23,9 @@ async function getTracks (term) {
     console.log(data[0].artist.name);
     console.log(data[0].album.image_url);
     // clear out old stuff before new stuff
-   document.querySelector('#tracks').innerHTML = "";
+    document.querySelector('#tracks').innerHTML = "";
 
-for(let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
 
         const template = ` <section class="track-item preview" onclick="playSong('${data[i].id}')">
         <img alt="This is an album cover for ${data[i].name}" src="${data[i].album.image_url}">
@@ -38,14 +38,14 @@ for(let i = 0; i < 5; i++){
         </div>
     </section>`;
 
-    document.querySelector('#tracks').insertAdjacentHTML('beforeend', template);
+        document.querySelector('#tracks').insertAdjacentHTML('beforeend', template);
     }
 };
 
 // The Job of playSong function is to create an iframe and 
 // replace the content in the #artist region withe the iframe
 
-function playSong(id){
+function playSong(id) {
     const iframe = `
             <iframe style="border-radius:12px" 
             src="https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0" 
@@ -56,40 +56,41 @@ function playSong(id){
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
             loading="lazy"></iframe>`;
 
-            document.querySelector('#artist').innerHTML = iframe;
+    document.querySelector('#artist').innerHTML = iframe;
 }
 
 
 
-async function getAlbums (term) {
-    const albumEndpoint = baseURL + "?q=" + term +"&type=album";
+async function getAlbums(term) {
+    const albumEndpoint = baseURL + "?q=" + term + "&type=album";
     console.log(albumEndpoint)
 
-    const data = await fetch(albumEndpoint).then(response => response.json());}
-   // console.log(data);
-   document.querySelector('#albums  ').innerHTML = "";
+    const data = await fetch(albumEndpoint).then(response => response.json());
+    console.log(data);
+    document.querySelector('#albums  ').innerHTML = "";
 
-   for(let i = 0; i < 5; i++){
+    for (let i = 0; i < data.length; i++) {
 
-    const template = ` <section class="album-card" id="2lATw9ZAVp7ILQcOKPCPqp">
-    <div>
-        <img alt="The album cover for ${data[i].name}" src="${data[i].image_url}">
-        <h2>${data[i].name}</h2>
-        <div class="footer">
-         <a href="${data[i].spotify_url}" target="_blank">
-       view on spotify
-          </a>
-        </div>
-    </div>
-</section>
-`;
+        const template = ` <section class="album-card" id="2lATw9ZAVp7ILQcOKPCPqp">
+            <div>
+                <img alt="The album cover for ${data[i].name}" src="${data[i].image_url}">
+                <h2>${data[i].name}</h2>
+                <div class="footer">
+                <a href="${data[i].spotify_url}" target="_blank">
+            view on spotify
+                </a>
+                </div>
+            </div>
+        </section>
+                `;
 
-    document.querySelector('#albums').insertAdjacentHTML('beforeend', template);
+        document.querySelector('#albums').insertAdjacentHTML('beforeend', template);
 
+    }
 };
 
-async function getArtist (term) {
-    const artistEndpoint = baseURL + "?q=" + term +"&type=artist";
+async function getArtist(term) {
+    const artistEndpoint = baseURL + "?q=" + term + "&type=artist";
     //console.log(artistEndpoint)
 
     const data = await fetch(artistEndpoint).then(response => response.json());
@@ -99,8 +100,8 @@ async function getArtist (term) {
         document.querySelector('#artist').innerHTML = "No results found.";
         return;
     }
-   // console.log(data);
-   // console.log(data[0].name);
+    // console.log(data);
+    // console.log(data[0].name);
     //console.log(data[0].image_url); 
 
     const template = `
@@ -110,7 +111,7 @@ async function getArtist (term) {
     <p>Popularity: ${data[0].popularity}</p>  
     </section>  
     `
-  //  console.log(template);
+    //  console.log(template);
 
     document.querySelector('#artist').innerHTML = template;
 
